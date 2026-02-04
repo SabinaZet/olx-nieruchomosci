1d5fac6c29b4eb7f965a4dcbed87b24066196c2b
Sabina Zet <sabinazekorn@gmail.com>
2026-02-04 20:53:06 +0100

    W main stworzenie listy ze słownikami zawierającymi data,meta,links dla każdej strony

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     page1 = pt.split_response(pr.request())
     total = int(page1.get('metadata').get('total_elements'))
     
-    pages = pr.pagination(total)
+    pages_l = pr.pagination(total)
+    pages = [pt.split_response(page) for page in pages_l]
+    pages.insert(0, page1)
