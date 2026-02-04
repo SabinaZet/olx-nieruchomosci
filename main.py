4e0ba06250cecece654c3df001ca30b06ebc8f6b
Sabina Zet <sabinazekorn@gmail.com>
2026-02-04 20:13:11 +0100

    W pipeline/transform func dzieląca response na dict z metadata,data i links. Potwierdzenie działania paginacji w pipeline/request. Dodanie obu func do main.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 if __name__ == '__main__':
     import pipeline.request as pr
+    import pipeline.transform as pt
     
-    r = pr.request()
+    page1 = pt.split_response(pr.request())
+    total = int(page1.get('metadata').get('total_elements'))
     
+    pages = pr.pagination(total)
---
+++ pipeline/transform.py
@@ -0,0 +1,15 @@
+def split_response(response: dict):
+    #lista słowników z produktami
+    data = response.get('data').get('clientCompatibleListings').get('data')
+
+    #słownik z metadanymi
+    metadata = response.get('data').get('clientCompatibleListings').get('metadata')
+
+    #słownik z linkami do następnej/poprzedniej strony
+    links = response.get('data').get('clientCompatibleListings').get('links')
+    
+    return {
+        'data':data,
+        'metadata':metadata,
+        'links':links
+        }
