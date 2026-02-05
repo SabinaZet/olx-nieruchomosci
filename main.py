e30ae654c2b3f37a329a534cce609ca11a444134
Sabina Zet <sabinazekorn@gmail.com>
2026-02-05 20:14:59 +0100

    Dodanie komentarzy do main

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ if __name__ == '__main__':
     import pipeline.request as pr
     import pipeline.transform as pt
     
+#list of dicts with scraped pages data,metadata and links
     pages = pr.paginate()
     
     data = pt.cumulate_data(pages)
-         
+    
+#dict with data split into categories(data_location,
+#data_offer,data_photos itp)
     separate_data = pt.data_separate(data)
     
+#dict with categories as keys and flattened DataFrames as values
     separate_dfs = pt.normalize_data(separate_data)
     pt.key_info(separate_dfs)
-        
+    
+#delete NaN columns from all dfs
     dfs_no_nan = pt.clean_nan(separate_dfs)
     pt.key_info(dfs_no_nan)
     
+#print df name and each column with the number of unique values
     pt.check_data(dfs_no_nan)
     
