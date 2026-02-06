9e0e24cf6ab1289aaff8e38934ff885428357c94
Sabina Zet <sabinazekorn@gmail.com>
2026-02-06 01:10:37 +0100

    Stworzenie pliku do czyszczenia danych pipeline/clean.py i przeniesienie tam funkcji do podglądania i czyszczenia danych.

---
+++ pipeline/clean.py
@@ -0,0 +1,64 @@
+"""Get insights from DataFrames stored in dict and clean
+it.
+
+Imports
+----------
+import pandas
+
+Functions
+----------
+-key_info(dfs: dict)
+-clean_nan(dfs: dict) -> dict
+-check_data(dfs: dict)
+"""
+
+import pandas as pd
+
+def key_info(dfs: dict):
+    """Print out df.info() for each DataFrame in dict.
+
+    Parameters
+    ----------
+    dfs : dict
+        DataFrames stored as dict values
+    """
+    for key in dfs.keys():
+        print()
+        print(key)
+        print(dfs[key].info())
+
+def clean_nan(dfs: dict) -> dict:
+    """Delete empty columns in all DataFrames stored in dict.
+
+    Parameters
+    ----------
+    dfs : dict
+        DataFrames stored as dict values
+        
+    Returns
+    ----------
+    dict
+        cleaned DataFrames as dict values
+    """
+    for name, df in dfs.items():
+        dfs[name] = df.dropna(axis=1, how="all")
+    return dfs
+
+def check_data(dfs: dict):
+    """Print number of unique values of each column for all
+    DataFrames stored in dict.
+
+    Prints notification if value is not a DataFrame.
+
+    Parameters
+    ----------
+    dfs : dict
+        DataFrames stored as dict values
+    """
+    for key, df in dfs.items():
+        print()
+        print(key)
+        try:
+            print(df.nunique())
+        except:
+            print('Lista')
