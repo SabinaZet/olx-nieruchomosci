9145e32d06dd53af8e7b9fb41309f80c6227e95d
Sabina Zet <sabinazekorn@gmail.com>
2026-02-06 01:09:22 +0100

    Skumulowanie funkcji do modyfikacji danych w jedną.

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 if __name__ == '__main__':
     import pipeline.request as pr
     import pipeline.transform as pt
+    import pipeline.clean as pc
     
 #list of dicts with scraped pages data,metadata and links
     pages = pr.paginate()
-#list with only data values from all pages
-    data = pt.cumulate_data(pages)
+
+#dict with data split into categories and DFs as values
+    data_categories = pt.normalized_data(pages)
     
-#dict with data split into categories(data_location,
-#data_offer,data_photos itp) by FIELD_MAP
-    separate_data = pt.data_separate(data)
+#data cleaning
     
-#dict with categories as keys and flattened DataFrames as values
-    separate_dfs = pt.normalize_data(separate_data)
-    pt.key_info(separate_dfs)
+#print df.info() for all dfs
+    pc.key_info(data_categories)
     
 #delete NaN columns from all dfs
-    dfs_no_nan = pt.clean_nan(separate_dfs)
-    pt.key_info(dfs_no_nan)
+    dfs_no_nan = pc.clean_nan(data_categories)
+    pc.key_info(dfs_no_nan)
     
 #print df name and each column with the number of unique values
-    pt.check_data(dfs_no_nan)
+    pc.check_data(dfs_no_nan)
     
